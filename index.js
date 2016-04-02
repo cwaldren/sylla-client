b084da70bd752a5268375bbd19b37e86c1ffcfe9
Casey Waldren <caseywaldren@gmail.com>
2016-04-02 13:51:00 -0400

    fixed my issues

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var PDF = Pdf;
 var App = React.createClass({
 	getInitialState: function() {
 		return {
-			file: 'sample-syllabus.pdf'
+			file: 'sample-syllabus.pdf',
+			files: []
 		};
 	},
 	onFileChange: function(ev) {
@@ -9,5 +11,24 @@ var App = React.createClass({
 			file: ev.target.files[0]
 		});
 	},
-	render: function()
-	
+	componentDidMount() {
+		this.serverRequest = $.get(this.props.source, function(result) {
+			this.setState({
+				files: result.pdfs
+			})
+		}.bind(this));	
+	},
+	componentWillUnmount: function() {
+		this.serverRequest.abort();
+	},
+	render: function(){
+		return (
+			<div className="pdf">
+				<PDF file={'pdfs/'+this.state.file} onDocumentComplete={this._onDocumentComplete}/>
+			</div>
+		)
+	},
+	_onDocumentComplete: function() {
+	}
+});
+ReactDOM.render(<App source="/api/v1.0/pdfs"/>, document.getElementById("container"));
